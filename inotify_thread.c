7a651ae3c4669fd835ef96c965df21b815860d98
tomer mizrachi <tomermizrachi@se.shenkar.ac.il>
2020-09-15 04:58:50 -0700

    begginning of inotify

---
+++ inotify_thread.c
@@ -0,0 +1,64 @@
+/*This is the sample program to notify us for the file creation and file deletion takes place in “/tmp” directory*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <linux/inotify.h>
+#include <time.h>
+
+#define EVENT_SIZE  ( sizeof (struct inotify_event) )
+#define EVENT_BUF_LEN     ( 1024 * ( EVENT_SIZE + 16 ) )
+
+void* inotify( void *arg )
+{
+  int length, i = 0;
+  int fd;
+  int wd;
+  char buffer[EVENT_BUF_LEN];
+  char* dir_path = (char *) arg;
+  time_t t = time(NULL);
+  struct tm tm = *localtime(&t);
+  char months[] = {"Jan", "Feb", "Mar", "Apr", "May",
+                       "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+  /*creating the INOTIFY instance*/
+  fd = inotify_init();
+
+  /*checking for error*/
+  if ( fd < 0 ) {
+    perror( "inotify_init" );
+  }
+
+  /*adding the “/tmp” directory into watch list. Here, the suggestion is to validate the existence of the directory before adding into monitoring list.*/
+  wd = inotify_add_watch( fd, dir_path , IN_ACCESS | IN_MODIFY );
+
+  /*read to determine the event change happens on path.dir. Actually this read blocks until the change event occurs*/ 
+
+  length = read( fd, buffer, EVENT_BUF_LEN ); 
+
+  /*checking for error*/
+  if ( length < 0 ) {
+    perror( "read" );
+  }  
+
+  /*actually read return the list of change events happens. Here, read the change event one by one and process it accordingly.*/
+  while ( i < length ) {     
+    struct inotify_event *event = ( struct inotify_event * ) &buffer[ i ];     
+    if ( event->len ) {
+        if ( event->mask & IN_ACCESS ) {
+            printf( "FILE ACCESSED: %s\nACCESS: READ\nTIME OF ACCESS: %02d %s %d: %02d:%02d\n", event->name, tm.tm_mday, months[tm.tm_mon], tm.tm_year + 1900,  tm.tm_hour, tm.tm_min);
+          }
+         else if ( event->mask & IN_MODIFY ) {
+            printf( "FILE ACCESSED: %s\nACCESS: WRITE\nTIME OF ACCESS: %02d %s %d: %02d:%02d\n", event->name, tm.tm_mday, months[tm.tm_mon], tm.tm_year + 1900,  tm.tm_hour, tm.tm_min);
+          }
+        } 
+      i += EVENT_SIZE + event->len;
+  }
+  /*removing the “/tmp” directory from the watch list.*/
+   inotify_rm_watch( fd, wd );
+
+  /*closing the INOTIFY instance*/
+   close( fd );
+
+}
--- myFileSystemMonitor.c
+++ myFileSystemMonitor.c
@@ -1,3 +1,4 @@
+#include <execinfo.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -7,31 +8,64 @@
 #include <pthread.h> // and link with -lpthread
 #include <libcli.h> // and link with -lcli.
 #include "telnet_thread.c"
+#include "inotify_thread.c"
 
 
+#define BT_BUF_SIZE 100
+
+void b_t_func(void)
+{
+	int j, nptrs;
+	void *buffer[BT_BUF_SIZE];
+	char **strings;
+
+	nptrs = backtrace(buffer, BT_BUF_SIZE);
+	printf("backtrace() returned %d addresses\n", nptrs);
+
+	/* The call backtrace_symbols_fd(buffer, nptrs, STDOUT_FILENO)
+	   would produce similar output to the following: */
+
+	strings = backtrace_symbols(buffer, nptrs);
+	if (strings == NULL) {
+		perror("backtrace_symbols");
+		exit(EXIT_FAILURE);
+	}
+
+	for (j = 0; j < nptrs; j++)
+		printf("%s\n", strings[j]);
+
+	free(strings);
+}
 
 int main(int argc, char *argv[])
 {
+  char opt;
+  char dir_path[100];
   pthread_t tid_telnet;
+  pthread_t tid_inotify;
+
   if (pthread_create(&tid_telnet, NULL, telnet, NULL))
 		return 1;
-  
-  	pthread_join(tid_telnet, NULL);
 
-  char opt;
-  
   while ((opt = getopt(argc, argv, "d:i:")) != -1)
   {
     switch (opt)
     {
     case 'd':
-    
-    break;
+      memset(dir_path, '\0', sizeof(dir_path));
+      strcat(dir_path, optarg);
+      if (pthread_create(&tid_telnet, NULL, inotify, (void*)dir_path))
+	    	return 1;
+      break;
     case 'i':
 
-    break;
+      break;
+    default:
+      break;
     }
-        
+      
   }
-  
+  pthread_join(tid_telnet, NULL);
+  pthread_join(tid_inotify, NULL);
+  return 0;  
 }
